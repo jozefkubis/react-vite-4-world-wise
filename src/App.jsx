6fdc0381fdea3c77cd575cb117c436e8d37dad7b
JozefK <164421631+jozefkubis@users.noreply.github.com>
2024-06-27 14:06:32 +0200

    Nested Routes and Index Route, Implementing the Cities List

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,51 @@ import Homepage from "./pages/Homepage"
 import Login from "./pages/Login"
 import AppLayout from "./pages/AppLayout"
 import PageNotFound from "./pages/PageNotFound"
+import CitiyList from "./components/CityList"
+import { useEffect, useState } from "react"
+
+const BASE_URL = "http://localhost:3500"
 
 function App() {
+  const [cities, setCities] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true)
+        const res = await fetch(`${BASE_URL}/cities`)
+        const data = await res.json()
+        setCities(data)
+      } catch {
+        alert("There was an error loading data...")
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchCities()
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CitiyList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CitiyList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
